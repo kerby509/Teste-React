da8009774cc846ef3e8f512e3ba5b6593f3dd684
kerby <lovincekerby99@gmail.com>
2022-10-10 14:23:15 -0300

    commit

--- react_front/src/Home/App.js
+++ react_front/src/Home/App.js
@@ -2,19 +2,53 @@ import React, {useState, useEffect} from "react";
 import {cience} from "../Components/Cience";
 import { Routes } from "../Components/root/Routers";
 import {technology} from "../Components/Technology";
+import { BrowserRouter as Rooter, Switch, Route, Link, Form } from "react-router-dom";
 
 
 
 
 function App  ()  {
     return (
-        <div> 
-          <h1>Pagina</h1>
+      <Rooter>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
 
-          {/* <Routes/> */}
-        
-        
-        </div>
+        <ul>
+          <li>
+            <Link to="/technology">TECHNOLOGY</Link>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <Link to="/cience">SCIENCE</Link>
+          </li>
+        </ul>
+
+        {/* <Switch>
+          <Route exact path="/">
+            <index />
+
+          </Route>
+
+          <Route path="/technology">
+            <technology />
+
+          </Route>
+
+          <Route path="/cience">
+            <cience />
+
+          </Route>
+
+
+
+        </Switch> */}
+      </Rooter>
+       
     )
 }
 
